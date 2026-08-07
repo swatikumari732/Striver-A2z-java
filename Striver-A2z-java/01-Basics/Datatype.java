aa220bf08759eb55bff136ca67226be792b491f7
SwatKumari732 <swatikumari1563@gmail.com>
2026-08-07 14:15:00 +0530

    Solved Datatypes

---
+++ Striver-A2z-java/01-Basics/Datatype.java
@@ -0,0 +1,21 @@
+public class Datatype {
+    public static void main(String[] args) {
+        String type= "Long";
+        if(type.equals("Integer")){
+            System.out.println(4);
+        }
+        else if(type.equals("Long")){
+           System.out.println(8);
+        }
+        else if(type.equals("Float")){
+           System.out.println(4);
+        }
+        else if(type.equals("Double")){
+            System.out.println(8);
+        }
+        else if(type.equals("Character")){
+          System.out.println(1);
+        }
+    }
+    
+}
